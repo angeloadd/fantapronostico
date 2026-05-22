75d86fdc0c210611e9dc2199be73333503f3e3d9
Angelo <angelo.adduci1991@gmail.com>
2026-05-22 13:51:35 +0200

    Update composer

--- app/Filament/Pages/Leagues.php
+++ app/Filament/Pages/Leagues.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 namespace App\Filament\Pages;
 
 use App\Filament\Widgets\LeaguesWidget;
+use BackedEnum;
 use Filament\Pages\Page;
 
 final class Leagues extends Page
 {
-    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+    protected static BackedEnum|string|null $navigationIcon = 'heroicon-o-document-text';
 
-    protected static string $view = 'filament.pages.leagues';
+    protected string $view = 'filament.pages.leagues';
 
     protected function getHeaderWidgets(): array
     {
