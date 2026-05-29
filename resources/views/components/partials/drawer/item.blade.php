9f71a0f05bb2760e43f9087b8c23089f769c2ab3
Angelo <angelo.adduci1991@gmail.com>
2026-05-29 13:01:20 +0200

    Fix item display in sidebar

--- resources/views/components/partials/drawer/item.blade.php
+++ resources/views/components/partials/drawer/item.blade.php
@@ -1,13 +1,14 @@
 @php
-    $isCurrentRoute = str_contains(Route::currentRouteName(), $active ?? $routeName)
+    $isCurrentRoute = str_contains(Route::currentRouteName(), $routeName)
 @endphp
 
-1<li class="p-1">
+<li>
     <a href="{{route($routeName)}}"
-        @class([
-             'bg-[#2b3b5a] text-accent border-l-accent border-l-2 rounded-2xl hover:text-base-100' => str_contains(Route::currentRouteName(), $active ?? $routeName),
-             'text-primary-content/85 hover:bg-[#2b3b5a]0' => !str_contains(Route::currentRouteName(), $active ?? $routeName)
-        ])
+            @class([
+                 'rounded-2xl',
+                 'bg-[#2b3b5a] text-accent border-l-accent border-l-2 hover:text-primary-content/85' => $isCurrentRoute,
+                 'text-primary-content/85 hover:bg-[#2b3b5a]' => !$isCurrentRoute,
+            ])
     >
         <x-dynamic-component :component="'partials.svgs.'.$svg"/>
         <span class="pl-1">{{$text}}</span>
--- resources/views/components/partials/drawer/side.blade.php
+++ resources/views/components/partials/drawer/side.blade.php
@@ -6,31 +6,30 @@
         </div>
         <div class="divider divider-neutral m-0"></div>
 
-        <ul class="menu text-lg w-full mb-4">
+        <ul class="menu text-lg w-full mb-4 space-y-1">
             <x-partials.drawer.item routeName="home" svg="home" text="Dashboard"/>
             <x-partials.drawer.item routeName="prediction.next-from-ref" active="prediction" svg="bet" text="Pronostico"/>
             <x-partials.drawer.item routeName="champion.create" active="champion" svg="winner" text="Vincente"/>
         </ul>
         <p class="text-primary-content/50 text-left w-full px-5 pt-5 text-sm">ESPLORA</p>
-        <ul class="menu text-lg w-full mb-4">
+        <ul class="menu text-lg w-full mb-4 space-y-1">
             <x-partials.drawer.item routeName="standing" svg="rank" text="Classifica"/>
             <x-partials.drawer.item routeName="albo" svg="albo" text="Albo d'oro"/>
             <x-partials.drawer.item routeName="terms" svg="terms" text="Regolamento"/>
         </ul>
-        @auth
-            @if(auth()->user()->mod)
-                <p class="text-primary-content/50 text-left w-full px-5 pt-5 text-sm">ADMIN</p>
-                <ul class="menu text-lg w-full">
-                    <li class="p-1">
-                        <a href="/admin"
-                           class="text-primary-content/85 hover:bg-[#2b3b5a]"
-                        >
-                            <x-dynamic-component :component="'partials.svgs.admin'"/>
-                            <span class="pl-1">Pannello Mod</span>
-                        </a>
-                    </li>
-                </ul>
-            @endif
-        @endauth
+
+        @if(auth()?->user()?->mod)
+            <p class="text-primary-content/50 text-left w-full px-5 pt-5 text-sm">ADMIN</p>
+            <ul class="menu text-lg w-full">
+                <li>
+                    <a href="/admin"
+                       class="text-primary-content/85 rounded-2xl hover:bg-[#2b3b5a]"
+                    >
+                        <x-dynamic-component :component="'partials.svgs.admin'"/>
+                        <span class="pl-1">Pannello Mod</span>
+                    </a>
+                </li>
+            </ul>
+        @endif
     </div>
 </aside>
