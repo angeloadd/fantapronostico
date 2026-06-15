a582bad113557b51cb0e9d272002397b8cf710f7
Angelo <angelo.adduci1991@gmail.com>
2026-06-15 11:49:50 +0200

    Fix login tabs text

--- app/Modules/Auth/Views/shared/tab.blade.php
+++ app/Modules/Auth/Views/shared/tab.blade.php
@@ -2,8 +2,8 @@
     href="{{route($name)}}"
     role="tab"
     @class([
-        'flex-1 text-center py-3 text-base-content transition-colors text-title',
-        'border-b-2 border-primary text-base-content font-semibold -mb-px' => Route::currentRouteName() === $name,
+        'flex-1 text-center py-3 text-base-content transition-colors text-title tracking-wide text-lg',
+        'border-b-2 border-primary text-base-content -mb-px' => Route::currentRouteName() === $name,
         'text-base-content/50 hover:text-base-content/80' => Route::currentRouteName() !== $name,
     ])
 >
